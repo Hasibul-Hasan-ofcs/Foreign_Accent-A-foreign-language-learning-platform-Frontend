09d091cdfe5a3bd18f13017f974626bd807c5dfa
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-06-09 06:31:27 +0600

    footer fix, 404 page update.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navigation/NavBar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-between">
       <NavBar></NavBar>
       <Outlet />
+      <Footer></Footer>
     </div>
   );
 }
--- src/pages/NotFoundErrorPage.jsx
+++ src/pages/NotFoundErrorPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
+import PNF from "../assets/img/404.png";
 
 const NotFoundErrorPage = () => {
   return (
@@ -9,11 +10,18 @@ const NotFoundErrorPage = () => {
         <title>404! page not found</title>
       </Helmet>
 
-      <div className="min-h-screen flex flex-col justify-center items-center gap-4">
-        <h1>404! Page not found</h1>
-        <Link to="/" className="btn btn-neutral">
-          &larr; Go Back to Homepage
-        </Link>
+      <div className="min-h-screen ">
+        <div className="container mx-auto flex flex-col items-center gap-4">
+          <div className="w-full flex justify-start py-8">
+            <Link to="/" className="btn btn-link  py-0">
+              &larr; Go Back to Homepage
+            </Link>
+          </div>
+          <img src={PNF} />
+          <h1 className="font-extrabold text-4xl text-center text-violet-800 ">
+            OOPS! Looks like the page you are looking for is not found!
+          </h1>
+        </div>
       </div>
     </>
   );
