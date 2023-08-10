2cd7be711d59970e2a77d328720e82bd57d98f30
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-08-10 22:53:48 +0600

    updated popular instructors

--- src/components/popular/CardPC.jsx
+++ src/components/popular/CardPC.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import { Bars } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
 
 const CardPC = ({ element }) => {
   const { theme, setTheme, user } = useContext(AuthContext);
@@ -59,39 +60,40 @@ const CardPC = ({ element }) => {
   // };
 
   return (
-    <div
-      className={`p_box_main shadow-md hover:shadow-xl rounded-xl cursor-pointer ${
-        theme === false ? "border-0" : "border"
-      } ${theme === false ? "bg-slate-700" : "bg-white"}`}
-    >
-      <div className="relative w-full aspect-img01 rounded-t-xl">
-        <div className="absolute top-4 right-4 z-10 inline-block rounded-2xl theme-bg text-white px-2">
-          {element?.available_seats} seats
+    <Link to="/classes/">
+      <div
+        className={`p_box_main shadow-md hover:shadow-xl rounded-xl cursor-pointer ${
+          theme === false ? "border-0" : "border"
+        } ${theme === false ? "bg-slate-700" : "bg-white"}`}
+      >
+        <div className="relative w-full aspect-img01 rounded-t-xl">
+          <div className="absolute top-4 right-4 z-10 inline-block rounded-2xl theme-bg text-white px-2">
+            {element?.available_seats} seats
+          </div>
+          <img
+            src={element?.class_image}
+            className="p_ins_img w-full aspect-img01 rounded-t-xl"
+          />
         </div>
-        <img
-          src={element?.class_image}
-          className="p_ins_img w-full aspect-img01 rounded-t-xl"
-        />
-      </div>
-      <div className="p-4">
-        <h2 className="theme-text text-2xl font-medium pt-2">
-          ${element?.price}
-        </h2>
-        <h2
-          className={`main_name text-gray-900 text-xl font-normal pt-2 line-clamp-2 ${
-            theme === false ? "text-slate-300" : "text-slate-500"
-          }`}
-        >
-          {element?.class_name}
-        </h2>
-        <p
-          className={`pt-1 text-sm text-gray-600 ${
-            theme === false ? "text-slate-300" : "text-slate-500"
-          }`}
-        >
-          {element?.instructor_name || "Instructor"}
-        </p>
-        {/* <p
+        <div className="p-4">
+          <h2 className="theme-text text-2xl font-medium pt-2">
+            ${element?.price}
+          </h2>
+          <h2
+            className={`main_name text-gray-900 text-xl font-normal pt-2 line-clamp-2 ${
+              theme === false ? "text-slate-300" : "text-slate-500"
+            } hover:underline transition-all`}
+          >
+            {element?.class_name}
+          </h2>
+          <p
+            className={`pt-1 text-sm text-gray-600 ${
+              theme === false ? "text-slate-300" : "text-slate-500"
+            }`}
+          >
+            {element?.instructor_name || "Instructor"}
+          </p>
+          {/* <p
         className={`pt-5 text-sm  line-clamp-3 ${
           theme === false ? "text-slate-400" : "text-slate-500"
         }`}
@@ -99,40 +101,40 @@ const CardPC = ({ element }) => {
         {element?.class_topics?.join(", ")}
       </p> */}
 
-        <div className="divider"></div>
-        <div className="flex justify-between">
-          <div className="flex gap-1 items-center">
-            <FaUsers className="theme-text"></FaUsers>
-            <span
-              className={`text-sm ${
-                theme === false ? "text-slate-400" : "text-slate-500"
-              }`}
-            >
-              {element?.students} Students
-            </span>
-          </div>
-          <div className="flex gap-1">
-            <FaStar className="text-amber-400"></FaStar>
-            <span
-              className={`text-sm ${
-                theme === false ? "text-slate-400" : "text-slate-500"
-              }`}
-            >
-              {element?.rating ? element?.rating : "4.5"}
-            </span>
+          <div className="divider"></div>
+          <div className="flex justify-between">
+            <div className="flex gap-1 items-center">
+              <FaUsers className="theme-text"></FaUsers>
+              <span
+                className={`text-sm ${
+                  theme === false ? "text-slate-400" : "text-slate-500"
+                }`}
+              >
+                {element?.students} Students
+              </span>
+            </div>
+            <div className="flex gap-1">
+              <FaStar className="text-amber-400"></FaStar>
+              <span
+                className={`text-sm ${
+                  theme === false ? "text-slate-400" : "text-slate-500"
+                }`}
+              >
+                {element?.rating ? element?.rating : "4.5"}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex justify-between">
-        {/* <div className="flex gap-1 items-center">
+        <div className="flex justify-between">
+          {/* <div className="flex gap-1 items-center">
           <div className="">
             <FaStar className="text-2xl theme-text"></FaStar>
           </div>
           <span className="text-sm text-gray-500">{element?.rating}</span>
         </div> */}
 
-        {/* <button
+          {/* <button
           onClick={handleSelectClass}
           className="btn rounded w-full text-white px-5 py-4 theme-bg hover:bg-green-500"
         >
@@ -152,8 +154,9 @@ const CardPC = ({ element }) => {
             <span>Select Class</span>
           )}
         </button> */}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
